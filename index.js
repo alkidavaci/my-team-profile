1862aa56b9ab5352ef4276f776c0dca8179036ea
Alkida Vaci <alkida.vaci@gmail.com>
2022-12-08 00:17:16 -0500

    Function prompt for Engineer

--- index.js
+++ index.js
@@ -69,11 +69,80 @@ const promptManager = () => {
         },
       ])
       .then((managerData) => {
+        // Destructuring assignment for manager inputs object
         const { name, id, email, officeNumber } = managerData;
-  
+        //Instantiation of Manager class 
         const teamMember = new Manager(name, id, email, officeNumber);
 
         members.push(teamMember);
                        
       });
+  };
+
+  // Prompt for Engineer
+  const promptEngineer = () => {
+    
+    return inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'What is your engineers name?',
+          validate: (nameInput) => {
+            if (nameInput) {
+              return true;
+            } else {
+              console.log(' Please enter your engineers name!');
+              return false;
+            }
+          },
+        },
+        {
+          type: 'input',
+          name: 'id',
+          message: 'Enter employee ID for the engineer:',
+          validate: (idInput) => {
+            if (!isNaN(idInput)) {
+              return true;
+            } else {
+              console.log(' Please enter an ID number!');
+              return false;
+            }
+          },
+        },
+        {
+          type: 'input',
+          name: 'email',
+          message: 'Enter email address for the engineer:',
+          validate: function (emailInput) {
+            const emailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+            if(!emailValid.test(emailInput)) {
+                return " Please enter a valid email address!"
+                }
+            return true
+            },
+        },
+        {
+          type: 'input',
+          name: 'githubUser',
+          message: 'Enter the engineers github username:',
+          validate: (githubInput) => {
+            if (githubInput) {
+              return true;
+            } else {
+              console.log(' Please enter a username!');
+              return false;
+            }
+          },
+        },
+      ])
+      .then((engineerData) => {
+        // Destructuring assignment for engineer inputs object
+        const { name, id, email, githubUser } = engineerData;
+        //Instantiation of Engineer class 
+        const teamMember = new Engineer (name, id, email, githubUser);
+  
+        members.push(teamMember);
+               
+    });
   };
